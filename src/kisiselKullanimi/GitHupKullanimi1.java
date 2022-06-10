f84b0a5660156e8f4cbee31454d22a2795d04a06
ibrahim <ibrahimdemirfr@gmail.com>
2022-06-10 22:03:36 +0200

    ilk degisiklik

--- src/kisiselKullanimi/GitHupKullanimi1.java
+++ src/kisiselKullanimi/GitHupKullanimi1.java
@@ -3,5 +3,7 @@ package kisiselKullanimi;
 public class GitHupKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+
+        System.out.println("iste bu kadar");
     }
 }
